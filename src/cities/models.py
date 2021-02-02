ab724b485d026861daeb9c2643113c8a2483af1b
Vlad Koliesov <kolesov0703@gmail.com>
2021-02-03 01:23:37 +0200

    first commit

---
+++ src/cities/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+class City(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+
+    
+    class Meta:
+        verbose_name = 'City'
+        verbose_name_plural = 'Cities'
+        ordering = ['name']
