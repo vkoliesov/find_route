94cf2e0284eb58e93be817896740c1c43aca1872
Vlad Koliesov <kolesov0703@gmail.com>
2021-02-23 20:22:44 +0200

    add style to pagination

--- src/cities/views.py
+++ src/cities/views.py
@@ -13,7 +13,6 @@ class CityListView(ListView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        print(context)
         form = CityForm()
         context['form'] = form
         return context
