c777a384abd53f41db28e671ccb3e684461811c6
vkoliesov <kolesov0703@gmail.com>
2021-03-11 17:53:04 +0200

    add trains view and templates

--- src/trains/views.py
+++ src/trains/views.py
@@ -1,3 +1,48 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from django.contrib import messages
+from django.contrib.messages.views import SuccessMessageMixin
 
-# Create your views here.
+from trains.models import Train
+
+
+class TrainListView(ListView):
+    model = Train
+    template_name = 'trains/home.html'
+    paginate_by = 5
+
+
+
+# class TrainDetailView(DetailView):
+#     queryset = Train.objects.all()
+#     template_name = 'trains/detail.html'
+
+#     # def get_queryset(self):
+#     #     return get_object_or_404(Train, pk=self.kwargs.get('pk'))
+
+
+# class TrainCreateView(CreateView):
+#     model = Train
+#     form_class = TrainForm
+#     template_name = 'trains/create.html'
+#     success_url = reverse_lazy('trains:home')
+#     success_message = "Train was created successfully"
+
+
+# class TrainUpdateView(SuccessMessageMixin, UpdateView):
+#     model = Train
+#     form_class = TrainForm
+#     template_name = 'trains/update.html'
+#     success_url = reverse_lazy('trains:home')
+#     success_message = "Train was edited successfully"
+
+
+# class TrainDeleteView(DeleteView):
+#     model = Train
+#     template_name = 'trains/delete.html'
+#     success_url = reverse_lazy('trains:home')
+
+#     def get(self, request, *args, **kwargs):
+#         messages.error(request, 'Train deleted.')
+#         return self.post(request, *args, **kwargs)
