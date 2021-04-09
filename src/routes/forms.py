1fe58ffd5526d3c96420569075000bf98e962236
vkoliesov <kolesov0703@gmail.com>
2021-04-09 16:21:11 +0300

    improvment routes form

--- src/routes/forms.py
+++ src/routes/forms.py
@@ -6,14 +6,14 @@ from cities.models import City
 class RouteForm(forms.Form):
 
     from_city = forms.ModelChoiceField(label='From', queryset=City.objects.all(),
-                                        widget=forms.Select(attrs={'class' : 'form-control'}))
+                                        widget=forms.Select(attrs={'class' : 'form-control js-example-basic-single'}))
 
     to_city = forms.ModelChoiceField(label='To', queryset=City.objects.all(),
-                                        widget=forms.Select(attrs={'class' : 'form-control'}))
+                                        widget=forms.Select(attrs={'class' : 'form-control js-example-basic-single'}))
 
     cities = forms.ModelMultipleChoiceField(
         label='Across cities', queryset=City.objects.all(),
-        required=False, widget=forms.SelectMultiple(attrs={'class' : 'form-control'
+        required=False, widget=forms.SelectMultiple(attrs={'class' : 'form-control js-example-basic-multiple'
     }))
 
     traveling_time = forms.IntegerField(label='Travel time', widget=forms.NumberInput(attrs={
