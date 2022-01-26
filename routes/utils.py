6b593010430752634e05551dab35954ab569270b
vk <vk@ucode.world>
2022-01-26 09:59:34 +0200

    completed find routes, fix some bugs

--- routes/utils.py
+++ routes/utils.py
@@ -9,8 +9,8 @@ def dfs_path(graph, start, goal):
             for next_ in graph[vertex] - set(path):
                 if next_ == goal:
                     yield path + [next_]
-        else:
-            stack.append((next_, path + [next_]))
+                else:
+                    stack.append((next_, path + [next_]))
 
 def get_graph(qs):
     graph = {}
@@ -21,7 +21,7 @@ def get_graph(qs):
 
 def get_routes(request, form):
     context = {'form': form}
-    qs = Train.objects.all()
+    qs = Train.objects.all().select_related('from_city', 'to_city')
     graph = get_graph(qs)
     data = form.cleaned_data
     from_city = data['from_city']
@@ -41,4 +41,44 @@ def get_routes(request, form):
                 right_ways.append(route)
         if not right_ways:
             raise ValueError('Route throught these cities is impossible')
+    else:
+        right_ways = all_ways
+    
+    routes = []
+    all_trains = {}
+
+    for q in qs:
+        all_trains.setdefault((q.from_city_id, q.to_city_id), [])
+        all_trains[(q.from_city_id, q.to_city_id)].append(q)
+
+    for route in right_ways:
+        tmp = {}
+        tmp['trains'] = []
+        total_time = 0
+        for i in range(len(route) - 1):
+            qs = all_trains[(route[i], route[i+1])]
+            q = qs[0]
+            total_time += q.travel_time
+            tmp['trains'].append(q)
+        tmp['total_time'] = total_time
+        if total_time <= traveling_time:
+            routes.append(tmp)
+
+    if not routes:
+        raise ValueError('Travele time is longer than indicated')
+
+    sorted_routes = []
+    if len(routes) == 1:
+        sorted_routes = routes
+    else:
+        times = list(set(r['total_time'] for r in routes))
+        times = sorted(times)
+
+        for time in times:
+            for route in routes:
+                if time == route['total_time']:
+                    sorted_routes.append(route)
+
+    context['routes'] = sorted_routes
+    context['cities'] = {'from_city': from_city.name, 'to_city': to_city.name}
     return context
